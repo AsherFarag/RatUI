77258836d01c6542305bdf4c3491455db8936c15
AsherFarag <AsherFarag@hotmail.com>
2026-03-18 11:27:50 +1100

    Create IWidget.h

---
+++ Include/RatUI/IWidget.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Core.h"
+#include "Layout.h"
+
+namespace RatUI
+{
+    class IWidget
+    {
+    public:
+        virtual ~IWidget() = default;
+
+        /**
+         * @brief Measures the desired size of the widget based on the given constraints.
+         * @param a_Constraints The size constraints to consider when measuring the widget.
+         * @return The desired size of the widget as a Vec2f, where x is the width and y is the height.
+         */
+        virtual Vec2f Measure( const Constraints& a_Constraints ) = 0;
+
+        /**
+         * @brief Arranges the widget within the given final rectangle, determining its final position and size.
+         * @param a_FinalRect The final rectangle within which the widget should be arranged, defined by its center and half-extents.
+         */
+        virtual void Arrange( const Rectf& a_FinalRect ) {}
+    };
+
+} // namespace RatUI
