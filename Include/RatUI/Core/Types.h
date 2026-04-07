1c3aa34ac8361c0acca6ae41b245b4ff1a401a5d
AsherFarag <AsherFarag@hotmail.com>
2026-04-07 22:54:28 +1000

    Update Types.h

--- Include/RatUI/Core/Types.h
+++ Include/RatUI/Core/Types.h
@@ -6,13 +6,17 @@ namespace RatUI
 {
     using f32 = float;
     using f64 = double;
-    
-    using i8 = std::int8_t;
+
+    using c8  = char;
+    using c16 = char16_t;
+    using c32 = char32_t;
+
+    using i8  = std::int8_t;
     using i16 = std::int16_t;
     using i32 = std::int32_t;
     using i64 = std::int64_t;
 
-    using u8 = std::uint8_t;
+    using u8  = std::uint8_t;
     using u16 = std::uint16_t;
     using u32 = std::uint32_t;
     using u64 = std::uint64_t;
