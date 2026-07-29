c409d5797967f6461fce4d378131e73fa395b717
AsherFarag <AsherFarag@hotmail.com>
2026-07-29 19:21:14 +1000

    [REFACTOR] Nuke tests
    Removed all tests as they are broken and useless. Needs to be rewritten

--- Tests/Layout/TestLayout.cpp
+++
@@ -1,6 +0,0 @@
-/**
- * @file TestLayout.cpp
- * @brief Tests for RatUI layout types: EAlign, ELayoutType, Visibility, Constraints, Edges, Anchor, LayoutStyle, LayoutResult.
- */
-
-#include "../Common/Common.h"
